d9e201d351f383aef94170b44c77411601754655
Jordan <jordan@graphemas.com>
2019-08-25 13:14:49 -0500

    Initial path solved

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get '/service-worker.js' => "service_worker#service_worker"
   get '/manifest.json' => "service_worker#manifest"
   get '/offline.html' => "service_worker#offline"
-  root to: "categories#index"
+  root to: "appoitments#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
